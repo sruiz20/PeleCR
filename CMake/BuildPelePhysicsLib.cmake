07f1c3cc00fa397a7fc10057912dcd3c5c4ccd4e
dmontgomeryNREL <David.Montgomery@nrel.gov>
2024-11-18 22:12:58 -0700

    Add path to CMake

--- CMake/BuildPelePhysicsLib.cmake
+++ CMake/BuildPelePhysicsLib.cmake
@@ -63,6 +63,12 @@ function(build_pele_physics_lib pele_physics_lib_name)
       ${PELE_PHYSICS_UTILITY_DIR}/Filter/Filter.H)
     target_include_directories(${pele_physics_lib_name} PUBLIC ${PELE_PHYSICS_UTILITY_DIR}/Filter)
 
+    target_sources(${pele_physics_lib_name}
+      PRIVATE
+      ${PELE_PHYSICS_UTILITY_DIR}/Utilities/UnitConversions.H
+      ${PELE_PHYSICS_UTILITY_DIR}/Utilities/Utilites.H)
+    target_include_directories(${pele_physics_lib_name} PUBLIC ${PELE_PHYSICS_UTILITY_DIR}/Utilities)
+
     target_sources(${pele_physics_lib_name} PRIVATE ${AMREX_SUNDIALS_DIR}/AMReX_Sundials.H
                                              ${AMREX_SUNDIALS_DIR}/AMReX_Sundials_Core.cpp
                                              ${AMREX_SUNDIALS_DIR}/AMReX_Sundials_Core.H
